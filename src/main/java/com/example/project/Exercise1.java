fa8123d043db06b95d589865e36b51c237ad19b7
Rutbel Carlos <rttitoca@unsa.edu.pe>
2022-08-06 18:04:42 -0500

    se agrego exercise 1 con metodos completos

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -7,28 +7,28 @@ public class Exercise1 {
     public static void main(String[] args) {
         Exercise1 obj = new Exercise1();
 
-        obj.tb = new HashLinearProbing(100);
+        obj.tb = new HashLinearProbing(10);
 
         obj.insertarPersona(new Persona("12345678", "Jorge Chamby"));
         obj.insertarPersona(new Persona("12345679", "Juan Perez"));
-
+        
         System.out.println(obj.encontrarPersona("12345678"));
-
+        
         obj.eliminarPersona("12345678");
-
+      
         System.out.println(obj.encontrarPersona("12345678"));
     }
 
     public void insertarPersona(Persona obj){
-        // Llamar al metodo 'insertHash' modificado
+        tb.insertHash(obj);
     }
 
     public void eliminarPersona(String dni){
-        // Llamar al metodo 'deleteHash' modificado
+        tb.deleteHash(dni);
     }
 
     // Retorna NULL quando no se encontro el dni, y el nombre de la persona si lo encontro
     public String encontrarPersona(String dni){
-        return null;
+        return tb.findHash(dni);
     }    
 }
